c351fad0f06b452a24bfc2035a8b34a6e2e05be9
dezinervishal <friends.forever618@gmail.com>
2020-01-03 22:44:32 +0530

    initial commit for react project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Welcome from "./components/welcome";
+import Well from "./components/well";
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const user = {
+//   firstName: "Vishal",
+//   lastName: "Chandola"
+// };
+
+// const formatName = user => user.firstName + " " + user.lastName;
+
+// const getGreeting = user => {
+//   if (user) {
+//     return <h1>Howdy, {formatName(user)}!</h1>;
+//   }
+
+//   return <h1>Howdy, Stranger</h1>;
+// };
+
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <div className="alert alert-success">{getGreeting(user)}</div>
+//     </div>
+//   );
+// };
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => (
+  <div className="Container">
+    <Welcome name="Vishal" />
+    <Welcome name="Manoj" />
+    <Welcome name="Mohit" />
+    <Welcome name="Vijay" />
+    <Well name="Deepika" />
+    <Well name="Rashmi" />
+    <Well name="Kavya" />
+    <Well name="Avni" />
+  </div>
+);
 
 export default App;
---
+++ src/components/well.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Well extends Component {
+  render() {
+    return <h1>Howdy, {this.props.name}</h1>;
+  }
+}
+
+export default Well;
